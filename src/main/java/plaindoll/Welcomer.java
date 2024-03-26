b4946a9303d0dfd075b18a38d64082a6a456bb6d
F145Hka <38522821+F145Hka@users.noreply.github.com>
2024-03-26 15:46:45 +0300

    Update Welcomer.java

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -13,4 +13,7 @@ public class Welcomer{
 	public String saySome(){
 		return "something in the way";
 	}
+	public String sayNeedGas(){
+                 return "Need more Vespene gas, hunter!";
+	}
 }
